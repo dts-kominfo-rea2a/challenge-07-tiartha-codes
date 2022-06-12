d7ffb7022b9410726f47fa62c24b2560d35db0fa
I Gusti Agung Sugihartha <igustiagungsugihartha@gmail.com>
2022-06-13 05:03:16 +0800

    fix: challenge07

--- promise.js
+++ promise.js
@@ -1,7 +1,14 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (emosi) => {
+  let ixx = await promiseTheaterIXX();
+  let vgc = await promiseTheaterVGC();
+  let ixxVgc = ixx.concat(vgc);
+  let totalMarahTidakMarah = 0;
+  ixxVgc.map(ixx => ixx.hasil === emosi && (totalMarahTidakMarah += 1))
+  return totalMarahTidakMarah
+};
 
 module.exports = {
   promiseOutput,
